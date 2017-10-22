c19cdac3726f6a42226beb1f3a134600b8250ac5
Christine <chrstn@uw.edu>
2017-10-21 21:37:16 -0700

    removed stuff that wasn't relevant

--- process.js
+++ process.js
@@ -2,68 +2,37 @@ function parse() {
   var elements = document.getElementsByTagName("img")
 
   for (var i = 0; i < elements.length; i++) {
-    console.log(elements[i]);
-    console.log(elements[i].src);
     processImage(elements[i].src, elements[i]);
-    // elements[i].alt = "hi";
   }
 
 }
 
 function processImage(source, element) {
-    // **********************************************
-    // *** Update or verify the following values. ***
-    // **********************************************
-
-    // Replace the subscriptionKey string value with your valid subscription key.
     var subscriptionKey = "20613855066847539021532e06e9ce11";
-
-    // Replace or verify the region.
-    //
-    // You must use the same region in your REST API call as you used to obtain your subscription keys.
-    // For example, if you obtained your subscription keys from the westus region, replace
-    // "westcentralus" in the URI below with "westus".
-    //
-    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-    // a free trial subscription key, you should not need to change this region.
     var uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
 
-    // Request parameters.
     var params = {
         "visualFeatures": "Categories,Description,Color",
         "details": "",
         "language": "en",
     };
 
-    // Display the image.
     var sourceImageUrl = source;
     document.querySelector("#sourceImage").src = sourceImageUrl;
 
-    // Perform the REST API call.
     $.ajax({
         url: uriBase + "?" + $.param(params),
-
-        // Request headers.
-        beforeSend: function(xhrObj){
+            beforeSend: function(xhrObj){
             xhrObj.setRequestHeader("Content-Type","application/json");
             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
         },
-
         type: "POST",
-
-        // Request body.
         data: '{"url": ' + '"' + sourceImageUrl + '"}',
     })
 
     .done(function(data) {
-        console.log(data);
-        console.log(data.description.captions[0].text);
         var ret= JSON.stringify(data.description.captions[0].text);
-        console.log(ret);
         element.alt = ret;
-        return ret;
-        // Show formatted JSON on webpage.
-        // $("#responseTextArea").val(JSON.stringify(data, null, 2));
     })
 
     .fail(function(jqXHR, textStatus, errorThrown) {
